290c791523780a91aa35d8e7908ce55f4c04e0f7
Thinnakorn R. <noogpee789@gmail.com>
2026-07-13 11:05:17 +0700

    Update App.js with css line

--- src/App.js
+++ src/App.js
@@ -9,6 +9,18 @@ function App() {
     name: rname,
     age: age
   };
+  const divStyle = {
+    color: 'blue',
+    backgroundColor: 'lightgray',
+    fontSize: 'larger',
+    padding: '3px',
+  };
+  return ( <div style={divStyle}>
+      Create Web & Mobile App <br />
+      with {user.name} <br />
+      Age is {user.age} <br />
+    </div>
+  );
 
   return (
     <div>

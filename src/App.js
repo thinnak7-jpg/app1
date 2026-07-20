958430c172c647d281e286bb1646eff1cec0c773
Thinnakorn R. <noogpee789@gmail.com>
2026-07-20 11:21:38 +0700

    update class-com and app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { Header, Content, Footer } from "./func-components";
-
+import Calendar from "./class-components";
 function App() {
     return (
         <>
+            
             <Header />
+            <p><center><Calendar /></center></p>
             <Content />
+            
             <Footer />
+            
         </>
     );
 }

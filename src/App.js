4ec463f58f03f518a066a42cfc774a4668501840
Thinnakorn R. <noogpee789@gmail.com>
2026-08-03 10:21:14 +0700

    new calculator

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React from "react";
 import { Header, Content, Footer } from "./func-components";
 import Calendar from "./class-components";
+import { Calculator2 } from "./calculator";
+
 function App() {
-    return (
-        <>
-            
-            <Header />
-            <p><center><Calendar /></center></p>
-            <Content />
-            
-            <Footer />
-            
-        </>
-    );
+  return (
+    <>
+      <Header />
+
+      <center>
+        <Calendar />
+      </center>
+
+      <Content />
+
+      <Calculator2 />
+
+      <Footer />
+    </>
+  );
 }
 
 export default App;
---
+++ src/calculator.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export function Calculator2() {
+  const n1 = 20;
+  const n2 = 10;
+
+  const calculate = (op) => {
+    const c = `${n1} ${op} ${n2}`;
+    const r = eval(c);
+    alert(`${c} = ${r}`);
+  };
+
+  return (
+    <div style={{ textAlign: 'center', marginTop: 20 }}>
+      <span>{n1}</span>&nbsp;
+
+      <button onClick={() => calculate('+')}>+</button>&nbsp;
+      <button onClick={() => calculate('-')}>-</button>&nbsp;
+      <button onClick={() => calculate('*')}>*</button>&nbsp;
+      <button onClick={() => calculate('/')}>/</button>&nbsp;
+
+      <span>{n2}</span>
+    </div>
+  );
+}

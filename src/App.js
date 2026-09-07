851709ee317384d9c4fe202aecea8de82aacc041
Thinnakorn R. <noogpee789@gmail.com>
2026-09-07 10:35:23 +0700

    update

--- src/App.js
+++ src/App.js
@@ -27,32 +27,9 @@
 //   );
 // }
 
-// export default App;
 import React from 'react'
-import { userContext } from './context'
-import Header from './context-header'
-import Content from './context-content'
-import Header2 from './context-header2'
-import Content2 from './context-content2'
+import Router2 from "./router2"
 
 export default function App() {
-    let [user, setUser] = React.useState('')
-
-    return (
-        <>
-            <userContext.Provider value={'Tom Jerry'}>
-                <Header/>
-                <Content/>
-            </userContext.Provider>
-
-            <br/>
-            <hr/>
-            <br/>
-
-            <userContext.Provider value={[user, setUser]}>
-                <Header2/>
-                <Content2/>
-            </userContext.Provider>
-        </>
-    )
+    return <Router2 />
 }
---
+++ src/home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+export default function Home() {
+return (
+<p style={{textAlign:'center'}}>
+<h3>ร้านค้าของ Thinnakorn</h3>
+รายชื่อ <a href="/products">สินค้า</a> ที่เรามีจำหน่าย<br/>
+แต่ท่านต้องเป็น <a href="/member">สมาชิก</a> จึงจะสั่งซื้อได้<br/>
+หากมีข้อสงสัย <a href="/contact">โปรดติดต่อเรา</a></p>
+)
+}
